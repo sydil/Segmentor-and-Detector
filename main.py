ac4b281d1d8018ece161a02278f10c34af6b6603
sydil <47665308+sydil@users.noreply.github.com>
2022-03-12 14:27:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,22 +7,20 @@ from keras import backend as K
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-data_gen_args = dict(rotation_range=0.2,
-                    width_shift_range=0.05,
+model = unet()
+model.fit_generator(trainGenerator(32,'concentrated/maskfits/maskpngs/train','image','label',
+                       rotation_range=0.2,
+                     rotation_range=1.50,
+                     rotation_range=0.90,
+                     width_shift_range=0.05,
+                     width_shift_range=0.2,
+                     width_shift_range=0.4,
                     height_shift_range=0.05,
+                    height_shift_range=0.2,
+                    height_shift_range=0.4,
                     shear_range=0.05,
                     zoom_range=0.05,
                     horizontal_flip=True,
-                    fill_mode='nearest')
+                    fill_mode='nearest,save_to_dir = None),steps_per_epoch=10,epochs=5)
+saveResult("/home/kupa/kupa/unetfiles/unet-Sydil/3C76/predict/",model.predict_generator(testGenerator("/home/kupa/kupa/unetfiles/unet-Sydil/3C76/images1/"),81,verbose=1))
 
-
-myGene = trainGenerator(32,'concentrated/maskfits/maskpngs/train','image','label',data_gen_args,save_to_dir = None)
-
-model = unet()
-model.fit_generator(myGene,steps_per_epoch=10,epochs=5)
-
-testGene = testGenerator("/home/kupa/kupa/unetfiles/unet-Sydil/3C76/images1/")
-results = model.predict_generator(testGene,81,verbose=1)
-saveResult("/home/kupa/kupa/unetfiles/unet-Sydil/3C76/predict/",results)
-print('prediction masks:', results.shape)
